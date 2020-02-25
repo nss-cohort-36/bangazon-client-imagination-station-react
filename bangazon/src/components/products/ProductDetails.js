a0f1d1f17bc32d9c1a1490fd4cf1f0f2b08e462b
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-25 11:19:05 -0600

    Added fetch call in ComponentDidMount for ProductDetail component to get product and update state. Added comments to the top documentation. Added comments to say what function needs to be written within the Component to handle the add to order button

--- bangazon/src/components/products/ProductDetails.js
+++ bangazon/src/components/products/ProductDetails.js
@@ -2,13 +2,17 @@
     ProductDetail.js
 
     Purpose:    This component is responsible for displaying the information
-                for a single product. 
+                for a single product. This handles functionality for adding a
+                product to an order.
 
     Author(s): Ryan Crowley
 */
 
 // REACT
 import React, { Component } from 'react'
+
+// API
+import APIManager from '../../modules/APIManager'
 
 
 class ProductDetail extends Component {
@@ -20,9 +24,22 @@ class ProductDetail extends Component {
         loadingStatus: true
     }
 
+    // Function to handle click event for add button. Adds product to order in database.
+    // Once completed, re-routes user to order view.
+
     componentDidMount() {
         // take id from props, fetch call to get product
-        // then, put info into state
+        APIManager.get("products", this.props.productId)
+            // then, put info into state
+            .then(productObject => {
+                this.setState({
+                    title: productObject.name,
+                    description: productObject.description,
+                    quantity: productObject.quantity,
+                    pricePerUnit: productObject.price,
+                    loadingStatus: false
+                })
+            })
     }
 
     render() {
@@ -35,10 +52,12 @@ class ProductDetail extends Component {
                     <h1>{this.state.title}</h1>
                     <p>{this.state.description}</p>
                     <ul>
-                        <li>Price per unit: {this.state.price}</li>
+                        <li>Price: {this.state.price}</li>
                         <li>Quantity available: {this.state.price}</li>
                     </ul>
+                    <button type="button">Add to Order</button>
                 </div>
+
 
             </ReactFragment>
         )
