665a467272021c2e796d57d645ab5bd85d011433
Trey Suiter <trey.suiter@gmail.com>
2020-02-25 10:34:37 -0600

    login and register creating tokens and updating db

--- bangazon/src/modules/simpleAuth.js
+++ bangazon/src/modules/simpleAuth.js
@@ -1,25 +1,25 @@
 const isAuthenticated = () => {
     return sessionStorage.getItem("bangazon_token") !== null
-  }
-  
-  const register = (userInfo) => {
+}
+
+const register = (userInfo) => {
     return fetch("http://localhost:8000/register", {
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-      },
-      body: JSON.stringify(userInfo)
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
     })
-    .then(res => res.json())
-    .then(res => {
-      if ("token" in res) {
-        sessionStorage.setItem( "bangazon_token", res.token )
-      }
-  })
-  }
-  
-  const login = (credentials) => {
+        .then(res => res.json())
+        .then(res => {
+            if ("token" in res) {
+                sessionStorage.setItem("bangazon_token", res.token)
+            }
+        })
+}
+
+const login = (credentials) => {
     return fetch("http://localhost:8000/login", {
         method: "POST",
         headers: {
@@ -28,17 +28,17 @@ const isAuthenticated = () => {
         },
         body: JSON.stringify(credentials)
     })
-    .then(res => res.json())
-    .then(res => {
-      if ("valid" in res && res.valid && "token" in res) {
-          sessionStorage.setItem( "bangazon_token", res.token )
-      }
-  })
-  }
-  
-  // Note! This is not an async method. Don't make the mistake of doing logout().then()
-  const logout = () => {
+        .then(res => res.json())
+        .then(res => {
+            if ("valid" in res && res.valid && "token" in res) {
+                sessionStorage.setItem("bangazon_token", res.token)
+            }
+        })
+}
+
+// Note! This is not an async method. Don't make the mistake of doing logout().then()
+const logout = () => {
     sessionStorage.removeItem("bangazon_token")
-  }
-  
-  export { isAuthenticated, login, register, logout }
+}
+
+export { isAuthenticated, login, register, logout }
