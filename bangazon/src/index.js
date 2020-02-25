33ac8f84dcf77e7a1605b0b35c228386b5e89454
Trey Suiter <trey.suiter@gmail.com>
2020-02-25 11:03:38 -0600

    login register test

--- bangazon/src/index.js
+++ bangazon/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Register from "./components/auth/Register"
+import App from "./App"
+import Register from "../src/components/auth/Register"
+import Login from "../src/components/auth/Login"
 
-ReactDOM.render(<Register />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
