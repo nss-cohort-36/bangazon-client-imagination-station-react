4332f051677e6a30b7d24fa636a3ea627496c0b6
Michelle Johnson <bob1234girl@gmail.com>
2020-02-27 09:26:32 -0600

    add styling to payment card and list

--- bangazon/src/components/payment/PaymentList.js
+++ bangazon/src/components/payment/PaymentList.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react"
 import Payment from "./PaymentCard"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 class PaymentList extends Component {
-// Creates the list for payment types
+  // Creates the list for payment types
   render() {
     return (
       <>
-        <article className="paymentList">
-          {
-            this.props.paymenttypes.map(type =>
-              <Payment
-                key={type.id}                
-                type={type}
-                deletePaymentType={this.props.deletePaymentType}
-              />)
-          }
-        </article>
+        <Card >
+          <CardContent>
+            <article className="paymentList">
+              {
+                this.props.paymenttypes.map(type =>
+                  <Payment
+                    key={type.id}
+                    type={type}
+                    deletePaymentType={this.props.deletePaymentType}
+                  />)
+              }
+            </article>
+          </CardContent>
+        </Card>
       </>
     )
   }
