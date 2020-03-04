5494fa242c2f5a1b32d45eb0b8c7123219d19feb
Michelle Johnson <bob1234girl@gmail.com>
2020-03-04 14:37:47 -0600

    changed component did mount for product types

--- bangazon/src/components/products/ProductCategories.js
+++ bangazon/src/components/products/ProductCategories.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -16,10 +16,39 @@ class ProductCategories extends Component {
         products: []
     }
 
-    render(){
-        return(
+    componentDidMount() {
+        APIManager.getAll("producttypes", "?customer=true")
+            .then((products) => {
+                console.log(products)
+                this.setState({
+                    products: products
+                })
+            })
+    }
+
+    render() {
+        return (
             <>
-                <Typography variant="h5" component="h1">Product Categories</Typography>
+                <Typography variant="h3" component="h1">Product Categories</Typography>
+                <Grid container spacing={16}>
+                    <Grid item xs={12} md={6}>
+                        {
+                            this.state.products.map(product =>
+                                <Typography variant="h5" >
+                                    {product.name}
+                                </Typography>
+                            )
+                        }
+                        <Typography variant="h6">
+                            Text only
+                        </Typography>
+                        <div>
+                            <List>
+                            </List>
+                        </div>
+                    </Grid>
+                </Grid>
+
             </>
         )
     }
