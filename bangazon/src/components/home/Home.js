5c3af6af7222ebef5cde4c464cf7e84ba27cbb6f
Lauren Riddle <lari7132@gmail.com>
2020-03-04 15:59:00 -0600

    populating newest prodcuts on home page

--- bangazon/src/components/home/Home.js
+++ bangazon/src/components/home/Home.js
@@ -1,28 +1,56 @@
 import APIManager from '../../modules/APIManager'
 import React, { Component } from 'react'
 import "./Home.css"
+import "../products/Product.css"
+import MyProductCard from "../products/MyProductCard"
 
 // Author: Lauren Riddle
-// Purpose: To display the thank you page after an order has been completed
+// Purpose: To display home page
 
-const Home = () => {
+class Home extends Component {
 
-    // render() {
+    state = {
+        products: [],
+    }
+
+    componentDidMount() {
+        APIManager.getAll("products")
+            .then((products) => {
+                let twentyproducts = []
+                let index = 0
+                products.map(product => {
+                    if (index < 20) {
+                        twentyproducts.push(product)
+                        index++
+                    }
+                })
+                this.setState({
+                    products: twentyproducts
+                })
+            })
+    }
+
+    render() {
 
         return (
             <>
-            
+
                 <div className="home-container">
-                    
-                <div className="smaller-home-container">
-                    <h1>Welcome to Bangazon Prime!</h1>
+
+                    <div className="smaller-home-container">
+                        <h1>Welcome to Bangazon Prime!</h1>
                     </div>
-                    
+
                 </div>
-                </>
-            
+                <section className="my-products-container products-home-container">
+                {this.state.products.map(product=> {
+                return <MyProductCard myProductCard={product} {...this.props}/>
+                })}
+                </section>
+            </>
+
         );
-    // }
+    }
 }
 
 export default Home
